48c0633dfca97feaf29de94af1dd5268fefb7884
TareqJudehGithub <tareq.judeh@outlook.com>
2021-06-09 18:09:19 +0200

    Finished for and while loops chapters and exercises in Lesson4.

--- Python/Lesson4/for_loop.py
+++ Python/Lesson4/for_loop.py
@@ -199,3 +199,109 @@ for word in book_title:
 
 print(another_word_counter)
 
+
+print('\n')
+'''
+Iterating Through Dictionaries with For Loops
+'''
+
+cast = {
+           "Jerry Seinfeld": "Jerry Seinfeld",
+           "Julia Louis-Dreyfus": "Elaine Benes",
+           "Jason Alexander": "George Costanza",
+           "Michael Richards": "Cosmo Kramer"
+       }
+# To iterate through both a dict keys and values, we use the .items() built in method:
+
+print('This example iterates through keys only')
+for actor in cast:
+    print(actor)
+
+print('')
+print('While this for loop example, uses a .items() method to iterate through both keys and values of a dict:')
+# We can even specify keys and items using two iteration variables:
+for k, v in cast.items():
+    print('Actor: {} -  Role: {}'.format(k, v))
+
+# You see, items() is an awesome method that returns tuples of key, value pairs, which we
+# can use to iterate over dictionaries in for loops.
+
+print('\n')
+#Quiz: Fruit Basket - Task 1
+
+items_no = 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+# Iterate through the dictionary
+for k, v in basket_items.items():
+    # if the key is in the list of fruits, add the value (number of fruits) to result
+    if k in fruits:
+        items_no += v
+
+print('')
+print('Result = {}'.format(items_no))
+
+print('\n')
+# Quiz: Fruit Basket - Task 2
+print('Quiz: Fruit Basket - Task 2')
+# Example 1
+
+result = 0
+basket_items = {'pears': 5, 'grapes': 19, 'kites': 3, 'sandwiches': 8, 'bananas': 4}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+# Your previous solution here
+for k, v in basket_items.items():
+    if k in fruits:
+        result += v
+
+print(result)
+
+print('')
+# Example 2
+
+result = 0
+basket_items = {'peaches': 5, 'lettuce': 2, 'kites': 3, 'sandwiches': 8, 'pears': 4}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+# Your previous solution here
+for k, v in basket_items.items():
+    if k in fruits:
+        result += v
+print(result)
+
+print('')
+# #Example 3
+
+result = 0
+basket_items = {'lettuce': 2, 'kites': 3, 'sandwiches': 8, 'pears': 4, 'bears': 10}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+# Your previous solution here
+for k, v in basket_items.items():
+    if k in fruits:
+        result += v
+print(result)
+
+
+print('\n')
+# You would like to count the number of fruits in your basket.
+# In order to do this, you have the following dictionary and list of
+# fruits.  Use the dictionary and list to count the total number
+# of fruits and not_fruits.
+
+fruit_count, not_fruit_count = 0, 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for k, v in basket_items.items():
+    # if the key is in the list of fruits, add to fruit_count.
+    if k in fruits:
+        fruit_count += v
+    # if the key is not in the list, then add to the not_fruit_count
+    else:
+        not_fruit_count += v
+
+print(fruit_count, not_fruit_count)
---
+++ Python/Lesson4/while_loop.py
@@ -0,0 +1,119 @@
+card_deck = [4, 11, 8, 5, 13, 2, 8, 10]
+hand = list()
+
+while sum(hand) <= 17:
+    hand.append(card_deck.pop())
+    print(f'Cards in hand; {hand}')
+    print(f'Sum of hands {sum(hand)}')
+
+print('')
+print(hand)
+
+'''
+Components of a While Loop
+- The first line starts with the while keyword, indicating this is a while loop.
+- Following that is a condition to be checked. In this example, that's sum(hand) <= 17
+- The while loop heading always ends with a colon :
+- Indented after this heading is the body of the while loop. If the condition for the 
+  while loop is true, the code lines in the loop's body will be executed.
+- We then go back to the while heading line, and the condition is evaluated again. This 
+  process of checking the condition and then executing the loop repeats until the condition 
+  becomes false. 
+- When the condition becomes false, we move on to the line following the body 
+  of the loop, which will be unindented.
+
+* The indented body of the loop should modify at least one variable in the test condition. If 
+  the value of the test condition never changes, the result is an infinite loop!
+'''
+
+print('\n')
+'''
+Practice: Factorials with While Loops
+Find the factorial of a number using a while loop.
+
+A factorial of a whole number is that number multiplied by every whole number between itself and 1. 
+For example, 6 factorial (written "6!") equals 6 x 5 x 4 x 3 x 2 x 1 = 720. So 6! = 720.
+'''
+# Using while loop
+number = 6
+product = 1
+current = 1
+
+while current <= number:
+    print(f'Current: {current}')
+    product *= current
+    current += 1
+    print(f'Product: {product}')
+
+print('')
+print(f'{number}! = {[product]}')
+
+
+print('\n')
+# Using for loop
+numbers = 5
+product = 1
+for i in range(1, 6):
+    print(product)
+    product *= i
+
+print('')
+print(product)
+
+
+print('\n')
+# Quiz: Count By
+
+start_num = 1 #provide some start number
+end_num = 10 #provide some end number that you stop when you hit
+count_by = 1 #provide some number to count by
+break_num = 0
+
+# write a while loop that uses break_num as the ongoing number to
+#   check against end_num
+
+if start_num > end_num:
+    result = "Oops! Looks like your start value is greater than the end value. Please try again."
+    print(result)
+else:
+    while break_num < end_num:
+        break_num += count_by
+    print(break_num)
+
+
+print('\n')
+
+# Quiz: Nearest Square
+limit = 40
+line = 1
+
+# write your while loop here
+
+while line ** 2 < limit:
+    nearest_square = line ** 2
+    line += 1
+
+print(nearest_square)
+
+print('\n')
+'''
+For Loops Vs. While Loops
+Now that you are familiar with both for and while loops, let's consider when it's most helpful to use each of them.
+
+for loops are ideal when the number of iterations is known or finite.
+
+Examples:
+
+When you have an iterable collection (list, string, set, tuple, dictionary)
+for name in names:
+When you want to iterate through a loop for a definite number of times, using range()
+for i in range(5):
+while loops are ideal when the iterations need to continue until a condition is met.
+
+Examples:
+
+When you want to use comparison operators
+while count <= 100:
+When you want to loop based on receiving specific user input.
+while user_input == 'y':
+'''
